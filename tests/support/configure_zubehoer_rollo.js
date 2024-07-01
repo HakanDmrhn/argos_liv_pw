dad040630f75b42b9efc33396c7bb364a25667df
HD <hakandemirhan@web.de>
2024-07-01 11:49:56 +0200

    RolloZubehoer remove Promise.all

--- tests/support/configure_zubehoer_rollo.js
+++ tests/support/configure_zubehoer_rollo.js
@@ -12,11 +12,6 @@ export async function configure_zubehoer_rollo(page) {
     //await page.waitForLoadState('load', { timeout: 10000 });
 
     // Wait for the response for JS files and check for status code 200
-    await Promise.all([
-        page.waitForResponse(response =>
-            response.url().includes('/skin/frontend/delphinus/default/js_minify/') &&
-            response.status() === 200, { timeout: 10000 }) // increased timeout to 10 seconds
-    ]);
 
     // Select size
     await page.locator('.last select').selectOption({ label: '100 cm' });
