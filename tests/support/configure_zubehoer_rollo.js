4b211b1746da451074fe7ecabc8b81331bd8c98f
HD <hakandemirhan@web.de>
2024-07-01 11:05:52 +0200

    RolloZubehoer update configurator rollo

--- tests/support/configure_zubehoer_rollo.js
+++ tests/support/configure_zubehoer_rollo.js
@@ -1,32 +1,31 @@
-import { expect } from '@playwright/test';
-
 export async function configure_zubehoer_rollo(page) {
-    // Load zubehör page
+
+    //load zubehör page
     await page.goto('/bedienstab-rollo-dachfenster');
+    await page.waitForLoadState('load', { timeout: 10000 }); // wait until the page loads
 
-   // Warte auf die Antwort für js-Dateien und überprüfe den Statuscode 200
-   // sonst entsteht JS-Error: opConfig not defined
 
+    // Warte auf die Antwort für js-Dateien und überprüfe den Statuscode 200
+    // sonst entsteht JS-Error: opConfig not defined
     await Promise.all([
         page.waitForResponse(response =>
             response.url().includes('/skin/frontend/delphinus/default/js_minify/')
-            && response.status() === 200, { timeout: 5000 })
+            && response.status() === 200, { timeout: 3000 })
     ]);
 
-    // Verify that the necessary elements are available and interact with them
-    const sizeSelector = page.locator('.last select');
-    await expect(sizeSelector).toBeVisible();
+    // promise not sufficient here!
+    // workaround
+    // await this.page.waitForTimeout(2000);
 
-    // Select size
-    await sizeSelector.selectOption({ label: '100 cm' });
+    //select size
+    const Bedienstab = page.locator('.last select');
+    await Bedienstab.toBeVisible();
+    await Bedienstab.toBeEnabled();
+    await Bedienstab.selectOption({ label: '100 cm' });
 
-    // Input quantity
-    const quantityInput = page.locator('#qty');
-    await expect(quantityInput).toBeVisible();
-    await quantityInput.fill('1');
+    // input quantity 
+    await page.locator('#qty').clear();
+    await page.locator('#qty').fill('1');
 
-    // Click add to cart button
-    const addToCartButton = page.locator('.cart-container > button');
-    await expect(addToCartButton).toBeVisible();
-    await addToCartButton.click();
+    await page.locator('.cart-container > button').click();
 }
