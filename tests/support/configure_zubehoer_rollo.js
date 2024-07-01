aeb46ad629f605e4fd696e476165850f8aada1d4
HakanDmrhn <167091148+HakanDmrhn@users.noreply.github.com>
2024-07-01 12:05:16 +0200

    Update configure_zubehoer_rollo.js

--- tests/support/configure_zubehoer_rollo.js
+++ tests/support/configure_zubehoer_rollo.js
@@ -1,26 +1,21 @@
+import { expect } from '@playwright/test';
+
 export async function configure_zubehoer_rollo(page) {
-
-    //load zubehör page
+    // Load Zubehör page
     await page.goto('/bedienstab-rollo-dachfenster');
+    
+   // ensure that the page has fully loaded by waiting for one of the last elements in network traffick 
+    const lastlink = page.getByRole('link', { name: 'Impressum' });
+    await expect(lastlink).toBeVisible();
+    await expect(lastlink).toBeEnabled();
 
-    // Warte auf die Antwort für js-Dateien und überprüfe den Statuscode 200
-    // sonst entsteht JS-Error: opConfig not defined
-    await Promise.all([
-        page.waitForResponse(response =>
-            response.url().includes('/skin/frontend/delphinus/default/js_minify/')
-            && response.status() === 200, { timeout: 3000 })
-    ]);
-
-    // promise not sufficient here!
-    // workaround
-    // await this.page.waitForTimeout(2000);
-
-    //select size
+    // Select size
     await page.locator('.last select').selectOption({ label: '100 cm' });
 
-    // input quantity 
+    // Input quantity 
     await page.locator('#qty').clear();
     await page.locator('#qty').fill('1');
 
+    // Click the add to cart button
     await page.locator('.cart-container > button').click();
 }
