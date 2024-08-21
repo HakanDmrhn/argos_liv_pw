b52d8f9043b67aa142927f0f968566d226682433
HD <hakandemirhan@web.de>
2024-08-21 13:27:40 +0200

    LIV-5860 fix freshchat helper

--- tests/support/helpers.js
+++ tests/support/helpers.js
@@ -20,25 +20,28 @@ export async function ignoreMenuContainer(page) {
 // --------------------------------------- FRESHCHAT ------------------------------------------//
 // --------------------------------------------------------------------------------------------//
 
-export async function ignoreFreshChat(page, attributeValue = 'transparent') {
+export async function ignoreFreshChat(page) {
     try {
-        // Define a locator for the FreshChat element
-        const freshChatIcon = await page.locator('#fc_frame').count();
+        // Define a locator for the FreshChat iframe
+        const freshChat = page.locator('#fc_frame');
 
-        if (freshChatIcon > 0) { // if this element exists
-            // Set the attribute to hide FreshChat
-            await page.evaluate((attrValue) => {
-                const freshChatElement = document.querySelector('#fc_frame');
-                if (freshChatElement) {
-                    freshChatElement.setAttribute('data-visual-test', attrValue);
-                }
-            }, attributeValue);
-        }
+        // Wait for the iframe to be visible
+        await freshChat.waitFor({ state: 'visible' });
 
+        // Evaluate the page context to modify the iframe attribute
+        await page.evaluate(() => {
+            const freshChatElement = document.querySelector('#fc_frame');
+            if (freshChatElement) {
+                freshChatElement.setAttribute('data-visual-test', 'transparent'); // Set attribute to 'transparent'
+            } else {
+                console.warn('FreshChat element not found.');
+            }
+        });
     } catch (error) {
-        console.error('Failed to ignore FreshChat:', error);
+        console.error('An error occurred while trying to ignore FreshChat:', error);
     }
 }
+
 
 // --------------------------------------------------------------------------------------------//
 // --------------------------------------- YOUTUBE --------------------------------------------//
