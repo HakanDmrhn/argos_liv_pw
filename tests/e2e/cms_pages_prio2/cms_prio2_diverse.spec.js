b54c66ff49052dafcc5c45bb582707d8cfc1aac5
HD <hakandemirhan@web.de>
2024-08-29 13:24:29 +0200

    LIV-5860 import helpers

--- tests/e2e/cms_pages_prio2/cms_prio2_diverse.spec.js
+++ tests/e2e/cms_pages_prio2/cms_prio2_diverse.spec.js
@@ -1,6 +1,6 @@
 import { argosScreenshot } from "@argos-ci/playwright";
 import { test } from '@playwright/test';
-import { ignoreFreshChat, ignoreYoutube, ignoreMenuContainer } from '../../support/helpers';
+import { ignoreFreshChat, ignoreYoutube, ignoreMenuContainer, checkButtonAvailability } from '../../support/helpers';
 var data = require("../../fixtures/cms_prio2_diverse.json");
 var pages = data.URLS;
 let scrollToBottom = require("scroll-to-bottomjs");
