c345b08bbfff84453783f7db322b90e1e8daecdd
HD <hakandemirhan@web.de>
2024-09-30 16:14:02 +0200

    LIV-5860 fix ratgeber  montage dachfenster

--- tests/e2e/cms_pages_prio2/cms_prio2_plissee.spec.js
+++ tests/e2e/cms_pages_prio2/cms_prio2_plissee.spec.js
@@ -1,5 +1,5 @@
 import { argosScreenshot } from '@argos-ci/playwright'
-import { test } from '@playwright/test'
+import { test, expect } from '@playwright/test'
 import { ignoreFreshChat, ignoreYoutube, ignoreMenuContainer, checkButtonAvailability } from '../../support/helpers'
 const data = require('../../fixtures/cms_prio2_plissee.json')
 const pages = data.URLS
@@ -8,13 +8,18 @@ const scrollToBottom = require('scroll-to-bottomjs')
 test.describe('Integration test with visual testing - Plissee CMS Prio2 pages', function () {
   pages.forEach(function (link) {
     test('Load page: ' + link + ' & take argos snapshot', async function ({ page }) {
-      await ignoreFreshChat(page)
       await ignoreYoutube(page)
+      await ignoreFreshChat(page)
       await page.goto(link, { waitUntil: 'load' })
       await page.waitForFunction(() => document.fonts.ready)
-      await page.evaluate(scrollToBottom)
       await checkButtonAvailability(page)
       await ignoreMenuContainer(page)
+
+      // ensure that the page has fully loaded by waiting for the logo c
+      const livoneoLogo = await page.getByRole('img', { name: 'Plissee und Sonnenschutz bei Livoneo®' })
+      await expect(livoneoLogo).toBeVisible()
+      await livoneoLogo.hover()
+      await page.mouse.move(0, 0)
 
       // take argos screenshot
       await argosScreenshot(page, link, {
